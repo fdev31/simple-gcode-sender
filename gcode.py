7c786a9171c0c8f585bf203b58d2d894f1a15422
fdev31 <fdev31@gmail.com>
2023-08-17 21:39:34 +0200

    implements a buffer

--- gcode.py
+++ gcode.py
@@ -11,41 +11,56 @@ ERROR_RETRY_DELAY = 0.5
 # TIMEOUT VALUE
 TIMEOUT = 0.2
 # ABSOLUTE MAX Loop count
-MAX_TIMEOUTS = int(2/TIMEOUT)
+MAX_TIMEOUTS = int(2 / TIMEOUT)
 # Retry enabled commands
 SAFE_COMMANDS = ["G0", "M5", "M9", "M30", "G28", "G53"]
 # length of the command queue waiting for "ok\r\n"
 BUFFER_SIZE = 10
+cmd_buffer = []
+replay_buffer = []
 
 
 def send_gcode_and_wait(ser, gcode_command, wait_for_ok=True, retries=0, expects=3):
     # Send the G-code command
     short_command = len(gcode_command) == 1
-    gcode_command += "\r\n"
-    ser.write(gcode_command.encode())
-    ser.flush()
+    gcode_command = gcode_command.encode() + b"\r\n"
+    ser.write(gcode_command)
+    cmd_buffer.append(gcode_command)
 
-    loopcount = count()
     response = b""
 
-    end_beacon = b"ok\r\nok\r\n" if short_command else b"ok\r\n"
-    while not response.endswith(end_beacon):
-        response += ser.read(expects)
+    if len(cmd_buffer) > BUFFER_SIZE:
+        loopcount = count()
 
-        if b"error" in response:
-            print(f"Error response: {response.decode()}")
-            if retries < MAX_RETRIES_SAFE:
-                print("Retrying...")
-                time.sleep(ERROR_RETRY_DELAY)  # Add a delay before retrying
-                return send_gcode_and_wait(ser, gcode_command, wait_for_ok, retries + 1)
-            else:
-                print("Max retries reached. Aborting.")
-                raise SystemExit()
-        if next(loopcount) > MAX_TIMEOUTS:
-            break
+        end_beacon = b"ok\r\nok\r\n" if short_command else b"ok\r\n"
+        while not response.endswith(end_beacon):
+            response += ser.read(expects)
 
+            if b"error" in response:
+                for i, resp in enumerate(response.split(b"\r\n")):
+                    if resp == b"ok":
+                        cmd_buffer.pop(0)
+                    elif resp.startswith(b"error"):
+                        replay_buffer.append(cmd_buffer.copy())
+            if next(loopcount) > MAX_TIMEOUTS:
+                break
+
+    cmd_buffer[0 : response.count(b"ok")] = []
     print(gcode_command, response)
     return response
+
+
+def gcode_iterator(fd):
+    for line in fd:
+        dx = line.find(";")  # strip comments
+        if dx >= 0:
+            line = line[:dx]
+        gcode_command = line.strip()
+        if not gcode_command:
+            continue
+        while replay_buffer:
+            yield replay_buffer.pop(0)
+        yield gcode_command
 
 
 def main():
@@ -69,21 +84,12 @@ def main():
 
         # Open the G-code file
         with open(gcode_filename, "r") as file:
-            for line in file:
-                dx = line.find(";")  # strip comments
-                if dx >= 0:
-                    line = line[:dx]
-                gcode_command = line.strip()
-                if not gcode_command:
-                    continue
-
-                # Retry safe commands
+            for gcode_command in gcode_iterator(file):
                 if any(gcode_command.startswith(cmd) for cmd in SAFE_COMMANDS):
                     send_gcode_and_wait(ser, gcode_command, retries=MAX_RETRIES_SAFE)
                 else:
                     send_gcode_and_wait(ser, gcode_command)
 
-        # Close the serial connection
         ser.close()
 
     except Exception as e:
