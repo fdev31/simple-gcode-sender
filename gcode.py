c02ce3693fbfbfae34abdbc318e53fd834fb4f59
fdev31 <fdev31@gmail.com>
2023-08-17 21:13:32 +0200

    timeouts support

--- gcode.py
+++ gcode.py
@@ -1,21 +1,36 @@
+#!./venv/bin/python
 import sys
 import serial
+from itertools import count
 import time
 
 # Maximum number of retries for safe commands
 MAX_RETRIES_SAFE = 3
 # Delay (in seconds) after encountering an error before retrying
 ERROR_RETRY_DELAY = 0.5
+# TIMEOUT VALUE
+TIMEOUT = 0.2
+# ABSOLUTE MAX Loop count
+MAX_TIMEOUTS = int(2/TIMEOUT)
+# Retry enabled commands
+SAFE_COMMANDS = ["G0", "M5", "M9", "M30", "G28", "G53"]
+# length of the command queue waiting for "ok\r\n"
+BUFFER_SIZE = 10
 
 
-def send_gcode_and_wait(ser, gcode_command, wait_for_ok=True, retries=0):
+def send_gcode_and_wait(ser, gcode_command, wait_for_ok=True, retries=0, expects=3):
     # Send the G-code command
-    ser.write((gcode_command + "\r\n").encode())
+    short_command = len(gcode_command) == 1
+    gcode_command += "\r\n"
+    ser.write(gcode_command.encode())
     ser.flush()
 
+    loopcount = count()
     response = b""
-    while not response.endswith(b"ok\r\n"):
-        response += ser.read()
+
+    end_beacon = b"ok\r\nok\r\n" if short_command else b"ok\r\n"
+    while not response.endswith(end_beacon):
+        response += ser.read(expects)
 
         if b"error" in response:
             print(f"Error response: {response.decode()}")
@@ -26,7 +41,10 @@ def send_gcode_and_wait(ser, gcode_command, wait_for_ok=True, retries=0):
             else:
                 print("Max retries reached. Aborting.")
                 raise SystemExit()
+        if next(loopcount) > MAX_TIMEOUTS:
+            break
 
+    print(gcode_command, response)
     return response
 
 
@@ -40,10 +58,12 @@ def main():
         serial_device = sys.argv[2]
 
         # Auto-detect and open the USB-serial device
-        ser = serial.Serial()
+        ser = serial.Serial(timeout=0.1)
         ser.baudrate = 115200  # Set the baud rate according to your device
         ser.port = serial_device
         ser.open()
+        # start fresh
+        ser.read(100)
         # Send the ~ command before starting engraving
         send_gcode_and_wait(ser, "~", retries=MAX_RETRIES_SAFE)  # Retry the "~" command
 
@@ -58,8 +78,7 @@ def main():
                     continue
 
                 # Retry safe commands
-                safe_commands = ["M5", "M9", "M30", "G28", "G53"]
-                if any(gcode_command.startswith(cmd) for cmd in safe_commands):
+                if any(gcode_command.startswith(cmd) for cmd in SAFE_COMMANDS):
                     send_gcode_and_wait(ser, gcode_command, retries=MAX_RETRIES_SAFE)
                 else:
                     send_gcode_and_wait(ser, gcode_command)
